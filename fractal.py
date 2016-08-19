178b16f1ce4dd364a6aca246907ac037de51239b
azev <lucasazevedo9@gmail.com>
2016-08-19 12:55:28 -0300

    level 2 ok

--- fractal.py
+++ fractal.py
@@ -14,24 +14,43 @@ def draw_square(side, fill=True):
 def draw_outer_square(side):
     draw_square(side, fill=False)
 
-def move_to_next_point(current_side):
-    forward(current_side)
-    left(90)
-    forward(current_side)
-    left(270)
+def move_to_next_point(current_level, current_side, current_square):
+    if current_square == 0:
+        forward(current_side)
+        left(90)
+        forward(current_side)
+        left(270)
+    elif current_square >= 1 and current_square <= 3:
+        if current_square == 3:
+            left(90)
+        forward(current_side*3)
+        if current_square == 3:
+            left(270)
+    elif current_square == 4: 
+        left(90)
+        forward(current_side*3)
+        left(270)
+    elif current_square == 5 or current_square == 6:
+        left(180)
+        forward(current_side*3)
+        left(180)
+    elif current_square == 7:
+        left(270)
+        forward(current_side*3)
+        left(90)
 
 def draw_level(current_level, outer_square_side):
     square_count = ((current_level-1)*9) + 1
-    current_side = outer_square_side/(current_level*3)
+    current_side = outer_square_side/(3**current_level)
+    print(current_side)
 
     for current_square in range(0, square_count):
-        move_to_next_point(current_side)
+        move_to_next_point(current_level, current_side, current_square)
         draw_square(current_side)
 
 def main():
-    levels = 1
-    first_square_side = 50
-    outer_square_side = 3*first_square_side 
+    levels = 2
+    outer_square_side = 300
 
     draw_outer_square(outer_square_side)
     home_point = pos()
@@ -43,4 +62,8 @@ def main():
     exitonclick()
 
 if __name__=='__main__':
+    # parse args : speed, outer square side
+
+    speed(0)
+    #speed("slowest")
     main()
